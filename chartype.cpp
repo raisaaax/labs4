c9cb0846f636414f29b92fb112a3b58225291486
Raisa Silea <silearaisa@gmail.com>
2023-10-27 10:51:08 +0100

    final commit

--- chartype.cpp
+++ chartype.cpp
@@ -1,18 +1,38 @@
 #include <iostream>
+#include <cctype>
 using namespace std;
 
-int isVowel(char ch){
+int isVowel(char ch) {
   string str = "aeiouAEIOU";
-  return(str.find(ch) != string::npos); 
+  return(str.find(ch) != string::npos);
+}	 
+
+int isPunct(char ch) {
+  string str = "!\"*,-.:;?";
+  return(str.find(ch) != string::npos);
 }
+
+int isDigit(char ch) {
+  string str = "0123456789";
+  return(str.find(ch) != string::npos);
+}
+
+
 
 int main() {
   char ch;
   cout << "Enter any character: ";
   cin >> ch;
-  if (isVowel(ch))
-    cout << ch << " is a Vowel" << endl;
-  else
-    cout << ch << " not a vowel" << endl;
+  if (isVowel(ch)) {
+    cout << ch << " is a vowel" << endl;
+  } else if (!(isVowel(ch))&& isalpha(ch) ) {
+    cout << ch << " is a consonant" << endl;
+  } else if (isDigit(ch)) {
+    cout << ch << " is a digit" << endl;
+  } else if (isPunct(ch)) {
+    cout << ch << " is punctuation" << endl;
+  } else {
+    cout << ch << " is unrecognised" << endl;
+  }
 }
 
