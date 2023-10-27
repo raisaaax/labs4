0b045e0973d2975921a4cb18ead3e83ec62e3f75
Raisa Silea <silearaisa@gmail.com>
2023-10-27 10:16:50 +0100

    3rd commin - checks vowels only

--- chartype.cpp
+++ chartype.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 using namespace std;
 
+int isVowel(char ch){
+  string str = "aeiouAEIOU";
+  return(str.find(ch) != string::npos); 
+}
+
 int main() {
   char ch;
   cout << "Enter any character: ";
   cin >> ch;
-  cout << "The ASCII of " << ch << "is" << int(ch);
+  if (isVowel(ch))
+    cout << ch << " is a Vowel" << endl;
+  else
+    cout << ch << " not a vowel" << endl;
 }
 
